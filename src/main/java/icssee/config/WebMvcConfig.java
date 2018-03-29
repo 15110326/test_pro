9924ac783a4e99f4d600a7a106d928df394008cc
HuynhThuan <15110326@student.hcmute.edu.vn>
2018-03-30 04:24:22 +0700

    hello

--- src/main/java/icssee/config/WebMvcConfig.java
+++ src/main/java/icssee/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -14,5 +15,14 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 		ReloadableResourceBundleMessageSource bean = new ReloadableResourceBundleMessageSource();
 		bean.addBasenames("classpath:messages");
 		return bean;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		//   file:D:\\data\\file\\image\\
+		registry.addResourceHandler("/public/image/**").addResourceLocations("classpath:/static/uploadmedia/");
+		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+		registry.addResourceHandler("/ckfinder/**").addResourceLocations("classpath:/static/ckfinder/");
+		super.addResourceHandlers(registry);
 	}
 }
